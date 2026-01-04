b8bf43c869e33f80a513297a2744d7da515a81e7
Ruxshona <mamatisaqovaruhshona@gmail.com>
2026-01-04 18:58:04 +0500

    Javascript_darslar

---
+++ dars1/dars_1.js
@@ -0,0 +1,44 @@
+// //misol-1
+
+// // let str1="hello";
+// // str1+="ruxshona";
+
+// // // let natija=str1.concat(" ",str2)
+// // console.log(str1);
+
+// console.log("Hello! I'm your coding fun fact guide!");
+// let botName="Ruxshona";
+// let botLocation="Andijan";
+// let favoriteLanguage="Python";
+
+// console.log("My name is "+botName+" and I live on "+botLocation+".");
+// console.log("My favorite programming language is "+favoriteLanguage+".");
+
+// let codingFact;
+// codingFact="my favorite "+favoriteLanguage;
+// console.log(codingFact);
+
+// favoriteLanguage="Java";
+// codingFact = "Actually, I prefer " + favoriteLanguage;
+// console.log(codingFact);
+
+// favoriteLanguage="Go";
+// codingFact="meni tilim "+favoriteLanguage+".";
+// console.log(codingFact);
+
+// console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
+
+
+
+
+// let adjective="sifat";
+// let noun="ot";
+// let verb="fel";
+// let place="joy";
+// let adjective2="sifat2";
+// let noun2="ot2";
+// let firstStory="Once upon a time, there was a(n) "+adjective+" "+noun+
+let hello = "Hello";
+hello += " world";
+console.log(hello);
+
---
+++ dars2/dars_2.js
@@ -0,0 +1,1 @@
+// Javascript boyicha masalalar;
